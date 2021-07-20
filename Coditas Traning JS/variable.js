0b13937e35100dd2db76d83cd8d971ad55ac3d41
AdhikarShinde <86773596+AdhikarShinde@users.noreply.github.com>
2021-07-20 10:53:55 +0530

    Add files via upload

---
+++ Coditas Traning JS/variable.js
@@ -0,0 +1,57 @@
+// var keyword
+
+var  primeNumber;  //Decelaration
+
+primeNumber = 11;  //Assignment
+
+var age = 22 ; //Decelaration + Assignment
+
+
+//Let Keyword
+
+let  primeNumber1;  //Decelaration
+
+primeNumber1 = 11;  //Assignment
+
+let age1 = 22 ; //Decelaration + Assignment
+
+
+//Let Keyword
+
+const PI = 3.14;  //Decelaration + Assignment is Compulsory
+
+// PI = 11;  //Invalid
+
+//const g ;
+ //g = 22 ; //Invalid
+
+//Js is a loosely typed and dynamic language 
+// example
+
+let name = "abcd";
+
+name = 9
+
+//Number
+//integers ,doubles, float, negatives
+//NaN - not a number
+
+
+const integer = 10
+
+console.log(typeof integer)
+
+//strings
+
+// String s1 = "Adhikar Shinde"
+// string s2 = 'Adhikar Shinde'
+// String s3 = `Adhikar Shinde` 
+
+//Boolean
+
+const value = true
+      
+ console.log(value)     
+
+
+ 
